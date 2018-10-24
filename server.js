7e205be0c841a94743c0d07404ca5fea16038877
mrbunited <mrbunited@gmail.com>
2018-10-24 16:52:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-
 const mongoose = require("mongoose");
 //const routes = require("./routes");
 const app = express();
@@ -10,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-=======
+
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -20,13 +18,13 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
->>>>>>> 7d47179175a65bd899306470d54bc98eb4e376f7
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-<<<<<<< HEAD
+
 //app.use(routes);
 
 // Connect to the Mongo DB
@@ -38,13 +36,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fauna_db");
 app.post('/registerSitter', function(res, req){
 
 });
-=======
+
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
->>>>>>> 7d47179175a65bd899306470d54bc98eb4e376f7
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
