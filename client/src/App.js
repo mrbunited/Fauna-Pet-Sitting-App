ae13fd0a3a63bf04f6ad0350544b6e3508a0e931
mradke2 <mrbunited@gmail.com>
2018-10-27 14:03:54 -0500

    google login

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,35 @@ import Nav from "./components/Nav"
 import SignIn from './components/SignIn';
 // import SignIn from "./components/SignIn"
 
+// import {Router} from 'react-router'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
+
+//App Layouts
+import Social from '../src/components/social/'
+import DragDrop from '../src/components/DragDrop/'
+import Jquery from '../src/components/Jquery/'
+//Import css for the application
+import './index.css'
+
 class App extends Component {
   render() {
     return (
+    
       <div>
         <Nav />
-        <SignIn></SignIn>
+              
+      <Router>
+
+      <SignIn>
+            {/* <Route exact path="/" title="FAUNA" component={Social}></Route>
+            <Route path="/social" component={Social}></Route>
+            <Route path="/dnd" component={DragDrop}></Route>
+            <Route path="/jquery" component={Jquery}></Route>
+             */}
+      </SignIn>
+        </Router>
+
       </div>
     );
   }
