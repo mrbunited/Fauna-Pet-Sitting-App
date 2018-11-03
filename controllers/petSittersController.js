ec4f3cc6fe95f2cc1c094cec1eb2c4a845110457
ahewardmills76 <ahewardmills76@gmail.com>
2018-11-03 12:19:46 -0500

    added findByAnimal route in PetSitters Controller

--- controllers/petSittersController.js
+++ controllers/petSittersController.js
@@ -15,6 +15,12 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findByAnimal: function(req,res) {
+    db.PetSitter
+    .find({})
+    .where('petExpertise').in([req.body.petExpertise])
+    .then(dbModels => res.json(dbModels))
+  },
   create: function(req, res) {
     console.log("In Create -------- ", req.body);
     db.PetSitter
