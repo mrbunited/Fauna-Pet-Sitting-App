1e036913416e8468e3c00de1f5e450a115f53381
mrbunited <mrbunited@gmail.com>
2018-11-08 18:15:01 -0600

    Update ProfilePetSitter.js

--- client/src/components/ProfilePetSitter/ProfilePetSitter.js
+++ client/src/components/ProfilePetSitter/ProfilePetSitter.js
@@ -67,7 +67,7 @@ class ProfilePetSitter extends React.Component {
     // this.getPetSitters();
   }
 
-static getPetSitters = () => {
+getPetSitters = () => {
     // console.log(animalType);
 
     API.getPetSitters()
